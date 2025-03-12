92c41070004f7031435d9c20d3c793df94f489ec
Thanh Tuyền <tuyen13022003@gmail.com>
2025-03-12 14:14:05 +0700

    fix css

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,26 +1,22 @@
-import * as Route from '@/constants/routes';
-import logo from '@/images/logo-full.png';
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import * as Route from "@/constants/routes";
+import logo from "@/images/logo-full.png";
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   const { pathname } = useLocation();
 
-  const visibleOnlyPath = [
-    Route.HOME,
-    Route.SHOP
-  ];
+  const visibleOnlyPath = [Route.HOME, Route.SHOP];
 
   return !visibleOnlyPath.includes(pathname) ? null : (
     <footer className="footer">
       <div className="footer-col-1">
         <strong>
-          <span>
-            Developed by
-            {' '}
-            <a href="https://github.com/punhe">PUNHE</a>
-          </span>
+          <span>Developed by EcoFit</span>
         </strong>
+        <p>Email: ecofit.cskh@gmail.com</p>
+        <p>Phone: 037 957 7352</p>
       </div>
       <div className="footer-col-2">
         <img alt="Footer logo" className="footer-logo" src={logo} />
@@ -30,8 +26,30 @@ const Footer = () => {
         </h5>
       </div>
       <div className="footer-col-3">
-        <strong>
-        </strong>
+        <p>Address: Thành phố Quy Nhơn - Bình Định</p>
+        <div className="social-links">
+          <a
+            href="https://www.facebook.com/profile.php?id=61572789724882"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFacebook />
+          </a>
+          <a
+            href="https://twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTwitter />
+          </a>
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram />
+          </a>
+        </div>
       </div>
     </footer>
   );
